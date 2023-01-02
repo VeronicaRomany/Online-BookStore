c09b5e4871be6b10814f640957ed6047f376fb9d
MarkEhab33 <markoehab33@gmail.com>
2023-01-02 23:18:55 +0200

    nav bar style

--- Front End/online-bookstore/src/app/app.module.ts
+++ Front End/online-bookstore/src/app/app.module.ts
@@ -4,21 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeadersComponent } from './shared/headers/headers.component';
 
-
-
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    HeadersComponent
+    HeadersComponent 
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
