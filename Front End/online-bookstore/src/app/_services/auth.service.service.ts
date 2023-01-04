982a8946c43e36ec711473bd969ddd0629eb3d61
Peteryasser <96436582+Peteryasser@users.noreply.github.com>
2023-01-04 20:30:00 +0200

    Login request

--- Front End/online-bookstore/src/app/_services/auth.service.service.ts
+++ Front End/online-bookstore/src/app/_services/auth.service.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { DataReturned } from '../dataReturned';
 
-const AUTH_API:string = 'http://localhost:8080/users/';
+const AUTH_API:string = 'http://localhost:8080/api/v1/user/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +12,12 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string) {
-    return this.http.post<DataReturned>(AUTH_API + 'signin', {
-      username,
-      password
-    },{
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    });
+    console.log(btoa(username+':'+password))
+
+    const httpOptions = {
+      headers: new HttpHeaders().append('Authorization','Basic cHk6MTIzNDU2')
+    };
+    console.log(httpOptions)
+    return this.http.post<DataReturned>(AUTH_API ,httpOptions);
   }
 }
