6fcf48875d3a2e204a9bff065d420e66171337b2
MarkEhab33 <markoehab33@gmail.com>
2023-01-04 16:58:31 +0200

    add to cart

--- Front End/online-bookstore/src/app/login/login.component.ts
+++ Front End/online-bookstore/src/app/login/login.component.ts
@@ -34,35 +34,35 @@ export class LoginComponent implements OnInit {
   onSubmit(): void {
     const { username, password } = this.form;
 
-    /*this.authService.login(username, password).subscribe((dataReturned)=> {
-        let data=dataReturned.id
-        let token=dataReturned.token
+  //   this.authService.login(username, password).subscribe((dataReturned)=> {
+  //       let data=dataReturned.isMgr
+  //       let token=dataReturned.token
 
-        if(data > 0){
-          this.tokenStorage.saveToken(username);
-          this.tokenStorage.saveUser({"username":username,"password":password,"userId":data,"token":token});
-          console.log(this.tokenStorage.getUser())
+  //       if(data > 0){
+  //         this.tokenStorage.saveToken(username);
+  //         this.tokenStorage.saveUser({"username":username,"password":password,"userId":data,"token":token});
+  //         console.log(this.tokenStorage.getUser())
 
     
-          this.isLoginFailed = false;
-          this.isLoggedIn = true;
-          this.username = this.tokenStorage.getUser().username;
-          this.router.navigate(['/', 'Home'])
-        }
-        else{
-          if(data == -1) {
-            this.errorMessage='Please enter valid username'
-            this.isLoginFailed = true;
-          }
-          if(data == -2) {
-            this.errorMessage='Please enter correct password'
-            this.isLoginFailed = true;
-          }
+  //         this.isLoginFailed = false;
+  //         this.isLoggedIn = true;
+  //         this.username = this.tokenStorage.getUser().username;
+  //         this.router.navigate(['/', 'Home'])
+  //       }
+  //       else{
+  //         if(data == -1) {
+  //           this.errorMessage='Please enter valid username'
+  //           this.isLoginFailed = true;
+  //         }
+  //         if(data == -2) {
+  //           this.errorMessage='Please enter correct password'
+  //           this.isLoginFailed = true;
+  //         }
 
-        }
-      },
-  );
-  this.reloadPage*/
+  //       }
+  //     },
+  // );
+  // this.reloadPage
   }
 
   reloadPage(): void {
